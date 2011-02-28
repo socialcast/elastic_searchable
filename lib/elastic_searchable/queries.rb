70e027cb04d948d515f3ec4eabe22fbd77c96363
Ryan Sonnek <ryan@socialcast.com>
2011-02-28 13:16:28 -0600

    reorg

--- lib/elastic_searchable/queries.rb
+++ lib/elastic_searchable/queries.rb
@@ -34,7 +34,8 @@ module ElasticSearchable
     # Model.max_per_page
     def per_page_for_search(options = {})
       per_page = options.delete(:per_page) || (self.respond_to?(:per_page) ? self.per_page : nil) || ElasticSearchable::Queries::PER_PAGE_DEFAULT
-      self.respond_to?(:max_per_page) ? [per_page.to_i, self.max_per_page].min : per_page
+      per_page = [per_page.to_i, self.max_per_page].min if self.respond_to?(:max_per_page)
+      per_page
     end
   end
 end
