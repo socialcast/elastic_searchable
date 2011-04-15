6969766a56e37fbf007e250132a0057f065cc2c1
Ryan Sonnek <ryan@socialcast.com>
2011-04-15 12:24:45 -0500

    simplify will_paginate initialization

--- test/setup_database.rb
+++ test/setup_database.rb
@@ -38,14 +38,11 @@ ActiveRecord::Schema.define(:version => 1) do
   end
 end
 
-begin
+if WillPaginate.respond_to?(:enable_activerecord)
+  puts 'configuring will_paginate v2.x'
   WillPaginate.enable_activerecord
-rescue => e
-  puts 'unable to configure will_paginate v2'
-end
-begin
+else
+  puts 'configuring will_paginate v3.x'
   require 'will_paginate/finders/active_record'
   WillPaginate::Finders::ActiveRecord.enable!
-rescue LoadError => e
-  puts 'unable to configure will_paginate v3'
 end
