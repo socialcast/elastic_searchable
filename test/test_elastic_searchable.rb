28d6114a08b98a138644ff40e205208847d82296
Michael Andrews <michael@socialcast.com>
2011-04-14 18:44:06 -0700

    Add unit test to exercise after_index_callback demultiplexing

--- test/test_elastic_searchable.rb
+++ test/test_elastic_searchable.rb
@@ -10,6 +10,7 @@ class TestElasticSearchable < Test::Unit::TestCase
     elastic_searchable :index_options => {'number_of_replicas' => 0, 'number_of_shards' => 1}
     after_index :indexed
     after_index_on_create :indexed_on_create
+    after_index_on_update :indexed_on_update
     def indexed
       @indexed = true
     end
@@ -21,6 +22,12 @@ class TestElasticSearchable < Test::Unit::TestCase
     end
     def indexed_on_create?
       @indexed_on_create
+    end
+    def indexed_on_update
+      @indexed_on_update = true
+    end
+    def indexed_on_update?
+      @indexed_on_update
     end
   end
   context 'activerecord class with default elastic_searchable config' do
@@ -63,6 +70,24 @@ class TestElasticSearchable < Test::Unit::TestCase
     end
     should 'have fired after_index_on_create callback' do
       assert @post.indexed_on_create?
+    end
+    should 'not have fired after_index_on_update callback' do
+      assert !@post.indexed_on_update?
+    end
+    context 'Model.update' do
+      setup do
+        @post.title = 'baz'
+        @post.save
+      end
+      should 'have fired after_index callback' do
+        assert @post.indexed?
+      end
+      should 'not have fired after_index_on_create callback' do
+        assert !@post.indexed_on_create?
+      end
+      should 'have fired after_index_on_update callback' do
+        assert @post.indexed_on_update?
+      end
     end
   end
 
