e84409c497022273e58b3a69445e1c88655be404
Dan Dockery <ddockery@vmware.com>
2011-11-11 15:23:36 -0600

    escape all lucene special characters, remove special case for !, update unit tests

--- test/test_elastic_searchable.rb
+++ test/test_elastic_searchable.rb
@@ -476,40 +476,82 @@ class TestElasticSearchable < Test::Unit::TestCase
         assert_equal '\!', result
       end
 
-      should "escape two exclamation marks" do
-        queryString = '!!'
-        result = ElasticSearchable.escape_query(queryString)
-        assert_equal '\!\!', result
-      end
-
-      should "escape five exclamation marks" do
-        queryString = '!!!!!'
-        result = ElasticSearchable.escape_query(queryString)
-        assert_equal '\!\!\!\!\!', result
-      end
-
-      should "leave exlamation at the beginning of a word intact" do
-        queryString = '!monkey'
-        result = ElasticSearchable.escape_query(queryString)
-        assert_equal '!monkey', result
-      end
-
-      should "escape exclamation mark before whitespace" do
-        queryString = '! '
-        result = ElasticSearchable.escape_query(queryString)
-        assert_equal '\! ', result
-      end
-
-      should "escape exclamation mark before closing parens" do
-        queryString = '!)'
-        result = ElasticSearchable.escape_query(queryString)
-        assert_equal '\!\)', result
-      end
-
       should "escape ^" do
         queryString = '^'
         result = ElasticSearchable.escape_query(queryString)
         assert_equal '\^', result
+      end
+
+      should "escape +" do
+        queryString = '+'
+        result = ElasticSearchable.escape_query(queryString)
+        assert_equal '\+', result
+      end
+
+      should "escape -" do
+        queryString = '-'
+        result = ElasticSearchable.escape_query(queryString)
+        assert_equal '\-', result
+      end
+
+      should "escape (" do
+        queryString = '('
+        result = ElasticSearchable.escape_query(queryString)
+        assert_equal '\(', result
+      end
+
+      should "escape )" do
+        queryString = ')'
+        result = ElasticSearchable.escape_query(queryString)
+        assert_equal '\)', result
+      end
+
+      should "escape {" do
+        queryString = '}'
+        result = ElasticSearchable.escape_query(queryString)
+        assert_equal '\}', result
+      end
+
+      should "escape [" do
+        queryString = '['
+        result = ElasticSearchable.escape_query(queryString)
+        assert_equal '\[', result
+      end
+
+      should "escape ]" do
+        queryString = ']'
+        result = ElasticSearchable.escape_query(queryString)
+        assert_equal '\]', result
+      end
+
+      should 'escape "' do
+        queryString = '"'
+        result = ElasticSearchable.escape_query(queryString)
+        assert_equal '\"', result
+      end
+
+      should "escape ~" do
+        queryString = '~'
+        result = ElasticSearchable.escape_query(queryString)
+        assert_equal '\~', result
+      end
+
+      should "escape *" do
+        queryString = '*'
+        result = ElasticSearchable.escape_query(queryString)
+        assert_equal '\*', result
+      end
+
+      should "escape :" do
+        queryString = ':'
+        result = ElasticSearchable.escape_query(queryString)
+        assert_equal '\:', result
+      end
+
+      should "escape ?" do
+        queryString = '?'
+        result = ElasticSearchable.escape_query(queryString)
+        assert_equal '\?', result
       end
     end
   end
