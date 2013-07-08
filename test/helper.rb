c24e65d67e93ff1dfbe2dd8cfb3a1da5b72baa64
Sean Walbran <swalbran@socialcast.com>
2013-07-08 16:09:32 -0500

    fix mocha deprecation and add deprecation logger

--- test/helper.rb
+++ test/helper.rb
@@ -9,13 +9,27 @@ rescue Bundler::BundlerError => e
 end
 require 'test/unit'
 require 'shoulda'
-require 'mocha'
+require 'mocha/setup'
 require 'pry'
 
 $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'elastic_searchable'
 require 'setup_database'
+
+class DeprecationLogger < Logger
+  def format_message(severity, timestamp, progname, msg)
+    "#{severity} #{msg}\n"
+  end
+end
+
+DEPRECATION_LOGGER = DeprecationLogger.new(File.join(File.dirname(__FILE__), "/deprecations.log"))
+ActiveSupport::Deprecation.debug = false
+ActiveSupport::Deprecation::DEFAULT_BEHAVIORS[:deprecation_log] = lambda { |message, callstack|
+  DEPRECATION_LOGGER.warn(message)
+#  DEPRECATION_LOGGER.warn(message + "\n\t" + callstack.join("\n\t"))
+}
+ActiveSupport::Deprecation.behavior = :deprecation_log
 
 class Test::Unit::TestCase
   def delete_index
